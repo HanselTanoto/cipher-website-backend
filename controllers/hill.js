82d55d5e3d31e9c67e5f1b855c99c2f852fa489c
hanseltanoto <13520046@std.stei.itb.ac.id>
2023-01-26 15:48:33 +0700

    feat: add hill encrypt decrypt

--- controllers/hill.js
+++ controllers/hill.js
@@ -1,5 +1,101 @@
-// hill cipher
+const MatrixInverse = require('matrix-inverse');
+
 module.exports = {
-    // hill cipher encryption function
     encrypt: (req, res) => {
-        l
+        let keySize = req.body.keySize;
+        let key = req.body.key;
+        let plaintext = req.text;
+
+        if (!keySize || !key) {
+            return res.status(400).send({
+                'err': 'Missing key'
+            });
+        }
+
+        plaintext = plaintext.replace(/[^\x00-\x7F]/g, '');
+        plaintext = plaintext.toLowerCase();
+
+        let ciphertext = '';
+
+        let plaintextNum = [];
+        for (let i = 0; i < plaintext.length; i++) {
+            plaintextNum.push(plaintext.charCodeAt(i) - 'a'.charCodeAt(0));
+        }
+
+        let keyMatrixNum = [];
+        for (let i = 0; i < keySize; i++) {
+            keyMatrixNum.push([]);
+            for (let j = 0; j < keySize; j++) {
+                keyMatrixNum[i].push(parseInt(key[i][j]));
+            }
+        }
+
+        for (let i = 0; i < plaintextNum.length; i += keySize) {
+            let temp = [];
+            for (let j = 0; j < keySize; j++) {
+                let sum = 0;
+                for (let k = 0; k < keySize; k++) {
+                    sum += keyMatrixNum[j][k] * plaintextNum[i + k];
+                }
+                temp.push(sum % 26);
+            }
+            for (let j = 0; j < keySize; j++) {
+                ciphertext += String.fromCharCode(temp[j] + 'a'.charCodeAt(0));
+            }
+        }
+
+        return res.status(200).send({
+            'cipher': ciphertext
+        });
+    },
+
+    decrypt: (req, res) => {
+        let keySize = req.body.keySize;
+        let key = req.body.key;
+        let ciphertext = req.text;
+        
+        if (!keySize || !key) {
+            return res.status(400).send({
+                'err': 'Missing key'
+            });
+        }
+
+        ciphertext = ciphertext.replace(/[^\x00-\x7F]/g, '');
+        ciphertext = ciphertext.toLowerCase();
+
+        let plaintext = '';
+
+        let ciphertextNum = [];
+        for (let i = 0; i < ciphertext.length; i++) {
+            ciphertextNum.push(ciphertext.charCodeAt(i) - 'a'.charCodeAt(0));
+        }
+
+        let keyMatrixNum = [];
+        for (let i = 0; i < keySize; i++) {
+            keyMatrixNum.push([]);
+            for (let j = 0; j < keySize; j++) {
+                keyMatrixNum[i].push(parseInt(key[i][j]));
+            }
+        }
+
+        let keyMatrixInverse = MatrixInverse(keyMatrixNum);
+
+        for (let i = 0; i < ciphertextNum.length; i += keySize) {
+            let temp = [];
+            for (let j = 0; j < keySize; j++) {
+                let sum = 0;
+                for (let k = 0; k < keySize; k++) {
+                    sum += keyMatrixInverse[j][k] * ciphertextNum[i + k];
+                }
+                temp.push(sum % 26);
+            }
+            for (let j = 0; j < keySize; j++) {
+                plaintext += String.fromCharCode(temp[j] + 'a'.charCodeAt(0));
+            }
+        }
+
+        return res.status(200).send({
+            'plain': plaintext
+        });
+    }   
+}
